db42325260c1d8f626d4ec553d266ca2a96b00da
John Clark <john@genealogysystems.com>
2013-12-10 22:25:52 +0000

    home page laid out

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,11 @@ module.exports = function(app){
 
   // Home landing page
   app.get('/', function(req, res){
-    res.render('home/home');
+    var data = {
+      css: ['/assets/css/home.css']
+    };
+  
+    res.render('home/home', data);
   });
 
 };
