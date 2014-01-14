d4f84c578f2f5e2ac87a84d47a508061aa7676d4
John Clark <john@genealogysystems.com>
2014-01-14 18:32:09 +0000

    added credentials file and centralized auth

--- routes/login.js
+++ routes/login.js
@@ -2,13 +2,12 @@
  * Setup login via HTTP Basic Auth at the
  * /auth/login URL
  */
+ 
+var restrict = require(__dirname + '/../lib/restrict.js');
+ 
 module.exports = function(app){
   
-  var auth = require('express').basicAuth(function(username, password){
-    return username == process.env.OPD_USERNAME && password == process.env.OPD_PASSWORD;
-  });
-  
-  app.get('/auth/login', auth, function(req, res){
+  app.get('/auth/login', restrict, function(req, res){
     res.cookie('opd-session', '1', {
       path: req.cookieOptions.path,
       domain: req.cookieOptions.domain,
